c0db5819cf5793ed310c576b2ea72fbbe3f062f4
Raj Goud <goudraj125@gmail.com>
2025-12-07 22:50:44 +0530

    create blog web with appwrite

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+
+  return (
+    <>
+      <h2>a blog web with appwrite</h2>
+    </>
+  )
+}
+
+export default App
---
+++ src/conf/conf.js
@@ -0,0 +1,10 @@
+const conf = {
+  APPWRITE_URL:String(import.meta.env.VITE_APPWRITE_URL),
+    PROJECT_ID: String(import.meta.env.VITE_SPPWRITE_PROJECT_ID),
+    DATABASE_ID: String(import.meta.env.VITE_SPPWRITE_DATABASE_ID),
+    COLLECTION_ID: String(import.meta.env.VITE_SPPWRITE_COLLECTION_ID),
+    BUCKET_ID: String(import.meta.env.VITE_SPPWRITE_BUCKET_ID),
+}
+
+
+export default conf
