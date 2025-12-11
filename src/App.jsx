bbbbad763fb639e4a5b610f1318b04b309c88e29
Raj Goud <goudraj125@gmail.com>
2025-12-11 21:31:16 +0530

    add some feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,42 @@
-
+import React, { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { login, logout } from './store/authSlice.js'
+import AuthService from './appwrite/auth'  
 import './App.css'
+import Header from './components/Header/Header.jsx'
+import Footer from './components/Footer/Footer.jsx'
+import { Outlet } from 'react-router-dom'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  // console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  
+  useEffect(() => {
+    AuthService.getCurrentUser()
+      .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout());
+       }
+      })
+    .finally(() => {
+      setLoading(false);
+    })
+  }, [])
 
-  return (
-    <>
-      <h2>a blog web with appwrite</h2>
-    </>
-  )
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+        demo :  {/* <Outlet/> */}
+        </main>
+        <Footer/>
+       </div>
+    </div>
+  ) : null;
 }
 
 export default App
